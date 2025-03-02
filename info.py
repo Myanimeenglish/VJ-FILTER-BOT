5b656b92260ae86f55318150e49faec11661d9b6
Myanimeenglish <myanimeenglishdub@gmail.com>
2025-03-02 09:43:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', '')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/ChillOutOTT')
+CHNL_LNK = environ.get('CHNL_LNK', '')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Businessdealsonly')
 
