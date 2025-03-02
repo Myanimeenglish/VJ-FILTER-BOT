c858d151dbbdb63d27f9f249dcba83367cb88c55
Myanimeenglish <myanimeenglishdub@gmail.com>
2025-03-02 09:19:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '20832372'))
 API_HASH = environ.get('API_HASH', 'a55cc8a30074a666afbd878866ebe617')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7592231700:AAG0kElRQLpT3s7PDtJDiVKjDI0BsfncK34")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
